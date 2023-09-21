4eeed9d0da80add315e6cd5b782cfe3ebd86ca86
LiamLime <contact@liamlime.com>
2023-09-21 13:13:44 +0100

    Initial commit of spring boot repository

---
+++ src/main/kotlin/com/liamlime/limefinance/LimeFinanceV2Application.kt
@@ -0,0 +1,11 @@
+package com.liamlime.limefinance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LimeFinanceV2Application
+
+fun main(args: Array<String>) {
+	runApplication<LimeFinanceV2Application>(*args)
+}
---
+++ src/test/kotlin/com/liamlime/limefinance/LimeFinanceV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.liamlime.limefinance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LimeFinanceV2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
